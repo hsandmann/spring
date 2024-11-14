8f9659c1bc9577762c592653e844c74a111fd05b
Humberto Sandmann <hsandmann@ieee.org>
2024-11-14 12:34:52 -0300

    cors

--- api/gateway-service/src/main/java/store/gateway/GatewayResource.java
+++ api/gateway-service/src/main/java/store/gateway/GatewayResource.java
@@ -1,7 +1,5 @@
 package store.gateway;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
